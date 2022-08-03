1aa2e888941e16894d7c9b37396f45874de2df78
marcelomaurin <marcelomaurinmartins@gmail.com>
2022-08-02 21:49:16 -0300

    teste

--- src/ouve.c
+++ src/ouve.c
@@ -10,6 +10,8 @@ Data: 15/06/2021
 #include<unistd.h>    //write
 #include <pocketsphinx.h>
 
+#include <sphinxbase/ad.h>
+#include <sphinxbase/err.h>
 
 
 #define PORTSRV 8097
@@ -17,33 +19,55 @@ Data: 15/06/2021
 ps_decoder_t *ps;
 cmd_ln_t *config;
 FILE *fh;
-char const *hyp, *uttid;
+//char const *hyp, *uttid;
+char const *hyp;                   // pointer to "hypothesis" (best guess at the decoded result)
+char const *decoded_speech;
+uint8 utt_started, in_speech;      // flags for tracking active speech - has speech started? - is speech currently happening?
+int32 k;                           // holds the number of frames in the audio buffer
+
+
+const char * recognize_from_microphone();
+int16 adbuf[4096];                 // buffer array to hold audio data
+
 int16 buf[512];
 int rv;
 int32 score;
+ad_rec_t *ad;                      // create audio recording structure - for use with ALSA functions
+
 
 void Ouvir(char * frase) {
-	Receive_Voice();
-	sprintf(frase,"Recognized: %s\n", hyp);
+	//hyp = Receive_Voice();
+	
+	
+	printf("Iniciou Ouvir\n");
+	decoded_speech = recognize_from_microphone();
+	printf("Hipo: %s\n", hyp);
+	printf("Recognized: %s\n",decoded_speech);
+	
+	printf("Finalizou Ouvir\n");
 }
 
+
 int Start_Voice(){
+	
 	config = cmd_ln_init(NULL, ps_args(), TRUE,
 		         "-hmm", MODELDIR "/en-us/en-us",
 		         "-lm", MODELDIR "/en-us/en-us.lm.bin",
 	    		 "-dict", MODELDIR "/en-us/cmudict-en-us.dict",
-		         NULL);
+			 NULL);
     if (config == NULL) {
 	fprintf(stderr, "Failed to create config object, see log for details\n");
 	return -1;
     }
 
     ps = ps_init(config);
+    ad = ad_open_dev("sysdefault", (int) cmd_ln_float32_r(config, "-samprate")); // open default microphone at default samplerate
+
     if (ps == NULL) {
 	fprintf(stderr, "Failed to create recognizer, see log for details\n");
 	return -1;
     }
-
+	/*
     fh = fopen("goforward.raw", "rb");
     if (fh == NULL) {
 	fprintf(stderr, "Unable to open input file goforward.raw\n");
@@ -52,31 +76,73 @@ int Start_Voice(){
 
     rv = ps_start_utt(ps);
 
-	return 0;
-	
+	//return 0;
+	*/
+	/*
+   config = cmd_ln_init(NULL, ps_args(), TRUE,                   // Load the configuration structure - ps_args() passes the default values
+			 "-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us",  // path to the standard english language model
+			 "-lm", "custom.lm",                                         // custom language model (file must be present)
+			 "-dict", "custom.dic",                                      // custom dictionary (file must be present)
+			 "-logfn", "/dev/null",                                      // suppress log info from being sent to screen
+			  NULL);
+
+   ps = ps_init(config);                                                        // initialize the pocketsphinx decoder
+   */
+   ad = ad_open_dev("sysdefault", (int) cmd_ln_float32_r(config, "-samprate")); // open default microphone at default samplerate
+   return 0;
 }
 
 
 int Receive_Voice(){
-	while (!feof(fh)) {
+    while (!feof(fh)) 
+    {
 		size_t nsamp;
 		nsamp = fread(buf, 2, 512, fh);
 		rv = ps_process_raw(ps, buf, nsamp, FALSE, FALSE);
     }
-	rv = ps_end_utt(ps);
+    rv = ps_end_utt(ps);
     hyp = ps_get_hyp(ps, &score);
 
 	return 0;
 
 }
 
-int Stop_Voice(){
+const char * recognize_from_microphone()
+{
+
+	ad_start_rec(ad);                                // start recording
+	ps_start_utt(ps);                                // mark the start of the utterance
+	utt_started = FALSE;                             // clear the utt_started flag
+	printf("Reconhecimento de microfone\n");
+	while(1) {                                       
+	    k = ad_read(ad, adbuf, 4096);                // capture the number of frames in the audio buffer
+            ps_process_raw(ps, adbuf, k, FALSE, FALSE);  // send the audio buffer to the pocketsphinx decoder
+	    in_speech = ps_get_in_speech(ps);            // test to see if speech is being detected
+
+            if (in_speech && !utt_started) {             // if speech has started and utt_started flag is false                           
+	        utt_started = TRUE;                      // then set the flag
+	    }
+
+	    if (!in_speech && utt_started) {             // if speech has ended and the utt_started flag is true
+                ps_end_utt(ps);                          // then mark the end of the utterance
+                ad_stop_rec(ad);                         // stop recording
+                hyp = ps_get_hyp(ps, NULL );             // query pocketsphinx for "hypothesis" of decoded statement
+                return hyp;                              // the function returns the hypothesis
+                break;                                   // exit the while loop and return to main
+            }
+       }
+}
 
 
-    fclose(fh);
-    ps_free(ps);
-    cmd_ln_free_r(config);
-	return 0;
+
+int Stop_Voice()
+{
+    printf("Parando captura\n");	
+    ad_close(ad); 
+    //close(fh);
+    //ps_free(ps);
+    //cmd_ln_free_r(config);
+    return 0;
 }
 
 int main(int argc , char *argv[])
@@ -110,8 +176,10 @@ int main(int argc , char *argv[])
     }
     puts("bind done");
     int flgAtivo = 1;
+    printf("Finalizando Start\n");
     while(flgAtivo==1)     
     {
+        printf("Flgativo\n");
 		//Listen
 		listen(socket_desc , 3);
      
@@ -119,7 +187,7 @@ int main(int argc , char *argv[])
 		puts("Waiting for incoming connections...");
 		c = sizeof(struct sockaddr_in);
 		//while(flgAtivo==1)
- 
+		
 		//accept connection from an incoming client
 		client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
 		if (client_sock < 0)
@@ -129,21 +197,25 @@ int main(int argc , char *argv[])
 		}
 		puts("Connection accepted");
     
-		//Receive a message from client
-		while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 ) {
-			memset(client_message,'\0',sizeof(client_message));
-			Ouvir(client_message);
-			printf("%s\n",client_message);
-		}
-     
-		if(read_size == 0)
+		while(1)
 		{
-			puts("Client disconnected");
-			fflush(stdout);
-		} else if(read_size == -1) {
-			perror("recv failed");
+			Ouvir(client_message);
+			/*
+			//Receive a message from client
+			while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 ) {
+				memset(client_message,'\0',sizeof(client_message));			
+				printf("%s\n",client_message);
+			}
+			*/
+			if(read_size == 0)
+			{
+				puts("Client disconnected");
+				fflush(stdout);
+			} else if(read_size == -1) {
+				perror("recv failed");
+			}
 		}
     }	 
-	Stop_Voice();
+    Stop_Voice();
     return 0;
 }

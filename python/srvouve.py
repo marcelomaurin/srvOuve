3e4ae268abf1bc2842589030b80afb2e3e6ff5cd
marcelomaurin <marcelomaurinmartins@gmail.com>
2024-08-12 00:11:10 +0100

    correcoes

--- python/srvouve.py
+++ python/srvouve.py
@@ -53,12 +53,21 @@ def loop():
     with sr.Microphone() as source:
         # Ajuste para o ruído de fundo fora do loop para não repetir a cada iteração
         print("Ajuste do ruído de fundo. Aguarde...")
-        r.adjust_for_ambient_noise(source)
+        r.adjust_for_ambient_noise(source, duration=1)
+        r.pause_threashold = 0.8
 
         while True:
+            # Ajustando o reconhecimento de fala para o ruído ambiente
+            print("Ajustando para o ruído ambiente...")
+            #r.adjust_for_ambient_noise(source, duration=1)
+
+            # Configurando o pause_threshold (tempo de espera por pausas na fala)
+            #r.pause_threshold = 0.8  # 0.8 segundos de pausa serão considerados como uma pausa na fala
+ 
             print("Fale algo:")
-            audio = r.listen(source)
+            audio = r.listen(source, phrase_time_limit=5)
             try:
+                print("Iniciou analise");
                 text = r.recognize_google(audio, language='pt-BR')
                 print("Você disse: " + text)
                 if last_said.full():  # Se a fila estiver cheia, remove o item mais antigo
